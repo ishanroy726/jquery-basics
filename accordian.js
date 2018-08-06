12c75778eb0e4e19d7ed38fc9dfb9cd5cdb1aafc
Ishan <ishanroy726@gmail.com>
2018-08-06 20:10:16 +0530

    first commit

---
+++ accordian.js
@@ -0,0 +1,12 @@
+
+
+
+
+$(document).ready(()=>{
+	$('.btn1').click(()=>{
+		$('.paragraph').hide();
+	});
+	$('.btn2').click(()=>{
+		$('.paragraph').show();
+	});
+})
---
+++ modal.js
@@ -0,0 +1,5 @@
+$(document).ready(()=>{
+	$("#btn").click(()=>{
+		$("#modal").css("display","block")
+	}) 
+})
